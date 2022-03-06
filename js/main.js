5b87c9a3c6992ea57b1340e000a8627526a0fcf6
Mansurbek-Eshbotayev <mustafoimron1820@gmail.com>
2022-03-06 21:03:12 +0500

    inital commit

---
+++ js/main.js
@@ -0,0 +1,70 @@
+
+// Birinchi mashq truelarni hisobini consolega chiqaring !
+
+// let arrey = [true, false,true, false,true, false,true, false,true, ]
+
+
+// function num(ar){
+// let sum = 0;
+// for(var i = 0; i < ar.length; i = i + 1){
+//      if( ar[i]===true ){
+//        sum = sum + 1
+//      }
+//     }
+//      return sum
+// }
+// console.log(num(arrey));
+
+
+
+
+
+// 2 chi mashq ( musbatlarni qo'shib consolega chiqaring !)
+
+// let arrey = [ -1,2, -4,3, -3,1, ]
+
+
+// function num(ar){
+// let sum = 0;
+// for(var i = 0; i < ar.length; i = i + 1){
+//      if( ar[i] > 0 ){
+//        sum = sum + ar[i]
+//      }
+//     }
+//      return sum
+// }
+// console.log(num(arrey));
+
+
+
+
+// 3 mashq (0 sonlarni consolda ohirida chiqsin string da !)
+// let arrey = [ 1, 3, 0, 3, 0, 7, 5, 0 ]
+
+
+// function num(ar){
+//  let num;
+// for(var a = 0; a < ar.length; a++){
+//   for(var b = a + 1; b < ar.length; b++)
+//      if( ar[a] === 0){
+//        num = ar[a];
+//        ar[a] = ar[b];
+//        ar[b] = num;
+//      }
+//     }
+//   let bum = ar.toString();
+//   bum = ar.join('');
+//      return bum
+// }
+// console.log(num(arrey));
+// console.log(num(typeof(arrey)));
+
+
+
+
+
+
+
+
+
+
